9683f2501ffaf57a704c51cb7687bd52d4b0a424
Replit user <>
2023-10-10 10:56:20 +0100

    first comit

--- index.js
+++ index.js
@@ -7,14 +7,29 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+//get multer
+var multer = require("multer")
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage })
 
+
+
+//INSERT NEW FILE
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  if(!req.file){return res.status(400).json({ message: 'No file uploaded' });}
+  
+  const fileName = req.file.originalname;
+  const fileType = req.file.mimetype;
+  const fileSize = Number(req.file.size);
+  res.json({ name: fileName, type: fileType, size: fileSize });
+})
 
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
